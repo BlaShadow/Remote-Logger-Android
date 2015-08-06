ed05c4e70af3ec9ef11d0611b5b42230ab7c9bf6
Blashadow <blackzerogamer@gmail.com>
2015-08-06 14:03:36 -0400

    gettter and setter added.

--- app/src/main/java/org/shadow/remoteloggerclient/domain/model/Server.java
+++ app/src/main/java/org/shadow/remoteloggerclient/domain/model/Server.java
@@ -11,10 +11,35 @@ import com.activeandroid.annotation.Table;
 @Table(name = "server")
 public class Server extends Model {
 
+    public Server(String name, String targetUrl){
+        super();
+
+        this.name = name;
+        this.targetUrl = targetUrl;
+    }
+
     @Column
     public String targetUrl;
 
+
     @Column
     public String name;
+
+    public String getTargetUrl() {
+        return targetUrl;
+    }
+
+    public void setTargetUrl(String targetUrl) {
+        this.targetUrl = targetUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 
 }
